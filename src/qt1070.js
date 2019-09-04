129b13cf46e48ead83ec3aec45bb3ee3b0ba6056
Nico De Witte <nico.dewitte@outlook.com>
2019-09-04 19:23:46 +0200

    Allow reading of chip id

--- src/qt1070.js
+++ src/qt1070.js
@@ -1,5 +1,5 @@
 const registers = {
-  CHIP_ID: 1,
+  CHIP_ID: 0,
   STATUS: 2,
   KEYSTATE: 3,
   LOW_POWER_MODE: 54,
@@ -9,9 +9,22 @@ const registers = {
 };
 
 class Qt1070 {
+  static ADDRESS = 0x1b;
 
   constructor(i2c) {
     this.i2c = i2c;
+  }
+
+  chip_id() {
+    return this.read_register(registers.CHIP_ID);
+  }
+
+  //////////////////////
+  // Internal methods //
+  //////////////////////
+
+  read_register(register) {
+    return this.i2c.readByteSync(Qt1070.ADDRESS, register);
   }
 
 }
